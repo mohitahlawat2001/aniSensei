39afd811485466987a49ae57c719585a3f9c428d
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2024-06-02 16:39:29 +0000

    added signIn/SignUp detail validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,31 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Header from "./Header";
+import { checkValidateData } from "../utils/checkValidateData";
 const Login = () => {
   const [isSignedInForm, setIsSignedInForm] = useState(true);
+  const [rememberMe, setRememberMe] = useState(false);
+  const [error, setError] = useState(null);
+
+  const name = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
+
   const toggleForm = () => {
     setIsSignedInForm(!isSignedInForm);
   };
+
+  const handleButtonCLick = () => {
+  //   console.log(email.current.value);
+  // console.log(password.current.value);
+  const msg = checkValidateData( name?.current?.value ,email.current.value, password.current.value);
+  setError(msg);
+
+  };
+
+  const handleRememberMe = () => {
+    setRememberMe(!rememberMe);
+  };
+  
   return (
     <div>
       <Header />
@@ -16,34 +37,49 @@ const Login = () => {
         />
       </div>
       <div className="absolute w-1/3 p-12 bg-black bg-opacity-80 my-36 mx-auto right-0 left-0 rounded-lg text-white">
-        <form className="flex flex-col">
+        <form 
+          onSubmit={(e) => e.preventDefault()}
+        >
           <h1 className="text-2xl  font-bold p-2 ">
             {isSignedInForm ? "Sign In" : "Sign Up"}
-        </h1>
-            {
-                !isSignedInForm && (
-                    <input
-                    type="text"
-                    placeholder="Name"
-                    className=" w-full h-10 bg-gray-700 bg-opacity-50 placeholder-white placeholder-opacity-50 rounded-md border-none focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50 my-4 mx-2 p-4"
-                    />
-                )
-
-            }
+          </h1>
+          {!isSignedInForm && (
+            <input
+              ref={name}
+              type="text"
+              placeholder="Name"
+              className=" w-full h-10 bg-gray-700 bg-opacity-50 placeholder-white placeholder-opacity-50 rounded-md border-none focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50 my-4 mx-2 p-4"
+            />
+          )}
           <input
+            ref={email}
             type="text"
             placeholder="Email or phone number"
             className=" w-full h-10 bg-gray-700 bg-opacity-50 placeholder-white placeholder-opacity-50 rounded-md border-none focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50 my-4 mx-2 p-4"
           />
           <input
+            ref={password}
             type="password"
             placeholder="Password"
             className=" w-full h-10 bg-gray-700 bg-opacity-50 placeholder-white placeholder-opacity-50 rounded-md border-none focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50 my-4 mx-2 p-4"
           />
-          <button className=" w-full h-10 bg-red-500 rounded-md border-none focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50  m-2 p-2 hover:bg-red-600 ">
+          {error && <p className="text-red-500 p-2">{error}</p>}
+          <button 
+          className=" w-full h-10 bg-red-500 rounded-md border-none focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50  m-2 p-2 hover:bg-red-600 "
+          onClick={handleButtonCLick}
+          >
             {isSignedInForm ? "Sign In" : "Sign Up"}
           </button>
-          <p  className='p-2 cursor-pointer ' onClick={toggleForm} >
+          <div className="flex items-center p-2">
+            <input 
+            type="checkbox" className="p-2 cursor-pointer accent-pink-300 focus:accent-red-500 "
+            onChange={handleRememberMe}
+             />
+            <p className="p-2">Remember me</p>
+          </div>
+          <hr className="border-white" />
+
+          <p className="p-2 cursor-pointer " onClick={toggleForm}>
             {isSignedInForm
               ? "New to Netflix? Sign up now."
               : "Already a member? Sign in now."}
